6b452792801e7a1b8a370a9de7fb5bdfa6b000f7
Avinash Chandiramani <avinash.chandir@gmail.com>
2023-05-16 16:40:34 -0600

    adding config to apps

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "anilist.apps.AnilistConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
