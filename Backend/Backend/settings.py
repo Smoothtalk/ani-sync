5eaecb01281af1d3e158fc52e30af2743015582a
Avinash Chandiramani <avinash.chandir@gmail.com>
2023-05-16 17:09:56 -0600

    change to postgres, password is a temp password and not secure. Will change to more robust implementation in the future

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ani-sync', 
+        'USER': 'postgres',
+        'PASSWORD': 'network',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
