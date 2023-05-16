125c088a175766a26e7513836be0d4b49162b661
Avinash Chandiramani <avinash.chandir@gmail.com>
2023-05-16 16:03:48 -0600

    creating models. Need to refactor relationships

--- Backend/anilist/models.py
+++ Backend/anilist/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    user_name = models.CharField(max_length=200)
-    custom_titles = models.DecimalField()
+    anilist_user_name = models.CharField(max_length=200)
+    custom_titles = models.JSONField()
 
 class Anime(models.Model):
+    show_id = 
+    title =
+    alt_title = models.JSONField()
+    status = 
+    last_watched_episode =
