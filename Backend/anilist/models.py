31b9d6c4c5a2df54e156ed100a75f907a0e43cc9
Avinash Chandiramani <avinash.chandir@gmail.com>
2023-10-19 11:40:26 -0600

    have internal status accurately reflect anilist backend

--- Backend/anilist/models.py
+++ Backend/anilist/models.py
@@ -1,29 +1,31 @@
 from django.db import models
 
+FINISHED = "FIN"
+RELEASING = "REL"
+NOT_YET_RELEASED = "NYR"
+CANCELLED = "CAN"
+HIATUS = "HIA"
+
+STATUS = [
+("FIN", "FINISHED"),
+("REL", "RELEASED"),
+("NYR", "NOT_YET_RELEASED"),
+("CAN", "CANCELLED"),
+("HIA", "HIATUS"),
+]
+
 # Create your models here.
 class AniList_User(models.Model):
     anilist_user_name = models.CharField(max_length=200)
 
 class Anime(models.Model):
-    FINISHED = "FIN"
-    RELEASING = "REL"
-    NOT_YET_RELEASED = "NYR"
-    CANCELLED = "CAN"
-    HIATUS = "HIA"
-
-    STATUS = [
-    ("FIN", "Finished"),
-    ("REL", "Releasing"),
-    ("NYR", "Not yet Released"),
-    ("CAN", "Cancelled"),
-    ("HIA", "Hiatus"),
-    ]
-
     show_id = models.IntegerField()
     title = models.CharField(max_length=300)
-    alt_title = models.JSONField()
+    alt_title = models.JSONField(blank=True)
     status = models.CharField(max_length=3, choices=STATUS, default=NOT_YET_RELEASED)
 
+    def convert_status(long_value):
+        print(dict(STATUS)[long_value])
 
 class User_Anime(models.Model):
     watcher = models.ForeignKey("AniList_User", on_delete=models.CASCADE)
