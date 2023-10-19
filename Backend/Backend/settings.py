22ac70b723c1a06b5ed409d5942d3cb4a8bc6c8f
Avinash Chandiramani <avinash.chandir@gmail.com>
2023-10-18 18:47:19 -0600

    testing urls

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "192.168.3.1", "127.0.0.1"]
 
 # Application definition
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
