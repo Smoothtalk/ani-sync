e5ad68fa00be37b911189c59cbddb8c7c2579d03
Avinash Chandiramani <avinash.chandir@gmail.com>
2024-09-11 13:25:52 -0600

    implement user_anime model

--- Backend/anilist/models.py
+++ Backend/anilist/models.py
@@ -6,13 +6,30 @@ NOT_YET_RELEASED = "NYR"
 CANCELLED = "CAN"
 HIATUS = "HIA"
 
-STATUS = [
+CURRENT = "CUR"
+PLANNING = "PLN"
+COMPLETED = "CPL"
+DROPPED = "DRP"
+PAUSED = "PAU"
+REPEATING = "RPR"
+
+AIRING_STATUS = [
 ("FIN", "FINISHED"),
 ("REL", "RELEASING"),
 ("NYR", "NOT_YET_RELEASED"),
 ("CAN", "CANCELLED"),
 ("HIA", "HIATUS"),
 ]
+
+WATCHING_STATUS = [
+("CUR", "CURRENT"),
+("PLN", "PLANNING"),
+("CPL", "COMPLETED"),
+("DRP", "DROPPED"),
+("PAU", "PAUSED"),
+("RPR", "REPEATING"),
+]
+
 
 # Create your models here.
 class AniList_User(models.Model):
@@ -29,10 +46,10 @@ class Anime(models.Model):
     show_id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=1000)
     alt_title = models.JSONField(blank=True)
-    status = models.CharField(max_length=3, choices=STATUS, default=NOT_YET_RELEASED)
+    status = models.CharField(max_length=3, choices=AIRING_STATUS, default=NOT_YET_RELEASED)
 
     def convert_status_to_db(long_value):
-        return [status for status in STATUS if status[1] == long_value][0][0]
+        return [status for status in AIRING_STATUS if status[1] == long_value][0][0]
     
     def __str__(self):
         return f"{self.title}"
@@ -43,8 +60,28 @@ class User_Anime(models.Model):
 
     watcher = models.ForeignKey("AniList_User", on_delete=models.CASCADE)
     show_id = models.ForeignKey("Anime", on_delete=models.CASCADE)
-    custom_titles = models.JSONField()
+    watching_status = models.CharField(max_length=3, choices=WATCHING_STATUS, default=PLANNING)
+    custom_titles = models.JSONField() #user added custom titles
     last_watched_episode = models.SmallIntegerField()
+
+    def convert_status_to_db(long_value):
+        return [status for status in WATCHING_STATUS if status[1] == long_value][0][0]
+
+    def __str__(self):
+        match self.watching_status:
+            case "CUR":
+                return f"{str(self.watcher) + " is watching " + str(self.show_id)}"
+            case "PLN":
+                return f"{str(self.watcher) + " is planning to watch " + str(self.show_id)}"
+            case "CPL":
+                return f"{str(self.watcher) + " has completed watching " + str(self.show_id)}"
+            case "DRP":
+                return f"{str(self.watcher) + " has dropped watching " + str(self.show_id)}"
+            case "PAU":
+                return f"{str(self.watcher) + " has paused watching " + str(self.show_id)}"
+            case "RPR":
+                return f"{str(self.watcher) + " is repeat watching " + str(self.show_id)}"
+
 
     class Meta:
         verbose_name = 'User_Anime'
