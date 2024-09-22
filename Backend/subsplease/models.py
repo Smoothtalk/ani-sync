3e6e224c8d70b0e6ab0497f2a2cbb518500a561f
Avinash Chandiramani <avinash.chandir@gmail.com>
2024-09-22 15:42:31 -0600

    more f string fixes

--- Backend/subsplease/models.py
+++ Backend/subsplease/models.py
@@ -7,7 +7,7 @@ class Url(models.Model):
     feed_url = models.CharField(max_length=200)
 
     def __str__(self):
-        return f"{self.feed_url.split("r=", 1)[1]}"
+        return f"{self.feed_url.split('r=', 1)[1]}"
 
 class Release(models.Model):
     full_title = models.CharField(max_length=400)
