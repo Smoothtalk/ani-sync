9bb5913f42debccdd656ab582a51af0039870832
Avinash Chandiramani <Avinash1010@gmail.com>
2024-11-08 16:41:29 +0000

    css styleing of new user form

--- Frontend/src/pages/NewUser.jsx
+++ Frontend/src/pages/NewUser.jsx
@@ -1,31 +1,39 @@
 import React, { useContext, useState } from "react";
 import { useNavigate, Navigate } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
-import style from "../components/css/login.module.css";
+import style from "../components/css/newuser.module.css";
 
 export default function NewUser() {
   const navigate = useNavigate();
-  const [value, setValue] = useState(""); //input field updating methods
-  const { user, setUser } = useContext(UserContext); //get user from context
+  const [inputNewUserName, setInputNewUsername] = useState(""); //input field updating methods
+  const [inputDiscordId, setInputDiscordId] = useState(""); //input field updating methods
+  const { setUser } = useContext(UserContext); //get user from context
 
   function handleSubmit(e) {
     e.preventDefault();
-    setUser(value);
+    setUser(inputNewUserName);
     navigate("/recent");
   }
 
   return (
-    <div className={style.loginDiv}>
-      <form className={style.loginForm} onSubmit={handleSubmit}>
+    <div className={style.newUserDiv}>
+      <form className={style.newUserForm} onSubmit={handleSubmit}>
         <input
-          className={style.loginInputUsername}
+          className={style.inputNewUsername}
           type="text"
           placeholder="Username"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
+          value={inputNewUserName}
+          onChange={(e) => setInputNewUsername(e.target.value)}
         ></input>
-        <button className={style.loginSubmitButton} type="submit">
-          Login
+        <input
+          className={style.loginDiscordId}
+          type="text"
+          placeholder="DiscordId (Optional)"
+          value={inputDiscordId}
+          onChange={(e) => setInputDiscordId(e.target.value)}
+        ></input>
+        <button className={style.newUserSubmitButton} type="submit">
+          Create New User
         </button>
       </form>
     </div>
