02bb34baa50a8919d91de0a87bc22994f5dc12af
Avinash Chandiramani <8347519+Smoothtalk@users.noreply.github.com>
2025-07-17 16:39:43 +0000

    fix scheduling command

--- Backend/Backend/management/commands/schedule.py
+++ Backend/Backend/management/commands/schedule.py
@@ -7,17 +7,29 @@ from Backend.tasks import run_sync_command  # Import the task function
 class Command(BaseCommand):
     help = 'Schedules the sync_anime_myo task every 30 minutes'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--user',
+            type=str,
+            required=True,
+            help='Username to run the sync for'
+        )
+
     def handle(self, *args, **kwargs):
+        sync_username = kwargs['user']
+        sync_anime_name = f'sync_anime_{sync_username}'
+
         # Check if the task is already scheduled
         from django_q.models import Schedule
-        if not Schedule.objects.filter(name='sync_anime_myo').exists():
+        if not Schedule.objects.filter(name='sync_anime').exists():
             # Schedule the task
             schedule(
                 func='Backend.tasks.run_sync_command',  # Fully qualified path to the function
                 schedule_type='I',  # Interval-based
                 minutes=30,  # Every 30 minutes
                 repeats=-1,  # Repeat indefinitely
-                name='sync_anime_myo'  # Unique name for this schedule
+                name=sync_anime_name,  # Unique name for this schedule
+                sync_user=sync_username 
             )
             self.stdout.write(self.style.SUCCESS('Scheduled sync_anime_myo successfully!'))
         else:
